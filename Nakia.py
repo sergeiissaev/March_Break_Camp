a80ae9b06c7f56106d62b3e0c0180c76c50712e7
Sergei Issaev <53531737+sergeiissaev@users.noreply.github.com>
2021-04-16 10:44:58 -0400

    Create Nakia.py

---
+++ Nakia.py
@@ -0,0 +1,76 @@
+print("Welcome friend, let's see how much you know about Canada!")
+print("1. Who was Canada's first prime minister?")
+
+print("Choose a Letter.\n A:Barack Obama\n B:Justin Trudeau \n C:John A. Macdonald  \n D:Alexander McDonald")
+Answer = (input("Enter Answer letter here:")).upper().strip()
+
+if (Answer == "A"):
+    print("Sorry, Inccorect. The correct answer was (C:John A. Macdonald). Keep Trying. Don't let this put you down")
+    
+elif (Answer == "B"):
+    print("Sorry, Inccorect. The correct answer was (C:John A. Macdonald).Keep Trying. Don't let this put you down")
+    
+elif (Answer == "C"):
+    print("Great job! you're off to a great start!")
+    
+elif (Answer == "D"):
+    print("Sorry, Inccorect. The correct answer was (C:John A. Macdonald). Keep Trying. Don't let this put you down.")
+    
+print("2. What year did Canada become a country?")
+
+print("Choose a Letter.\n A:1999 \n B:1756 \n C:2020  \n D:1867")
+Answer = (input("Enter Answer letter here:")).upper()
+
+if (Answer == "A"):
+    print("Sorry, Inccorect. The correct answer was (D:1867). Keep Trying. Don't let this put you down")
+    
+elif (Answer == "B"):
+    print("Sorry, Inccorect. The correct answer was (D:1867).Keep Trying. Don't let this put you down")
+    
+elif (Answer == "C"):
+    print("Sorry, Inccorect. The correct answer was (D:1867).Keep Trying. Don't let this put you down")
+    
+elif (Answer == "D"):
+    print("Great job! Keep up the great work!")
+    
+print("3. What is the current population of Canada?")
+
+print("Choose a Letter.\n A:53,001,200 \n B:38,006,691 \n C:32,687,988  \n D:550,670,403")
+Answer = (input("Enter Answer letter here:")).upper().strip()
+
+if (Answer == "A"):
+    print("Sorry, Inccorect. The correct answer was (B:38,006,691). Keep Trying. Don't let this put you down")
+    
+elif (Answer == "B"):
+    print("Great job! Keep up the great work!")
+    
+elif (Answer == "C"):
+    print("Sorry, Inccorect. The correct answer was (B:38,006,691).Keep Trying. Don't let this put you down")
+    
+elif (Answer == "D"):
+    print("Sorry, Inccorect. The correct answer was (B:38,006,691).Keep Trying. Don't let this put you down")
+    
+print("4. Canada has 1 official language.")
+
+print("Choose a Letter.\n A:True \n B:False")
+Answer = (input("Enter Answer letter here:")).upper().strip()
+
+if (Answer == "A"):
+    print("Sorry, Inccorect. The correct answer was (B:False). Keep Trying. Don't let this put you down.")
+    
+elif (Answer == "B"):
+    print("Great job. Keep up the great work.")
+    
+print("4. Canada has 2 official languages (French and English).")
+
+print("Choose a Letter.\n A:True \n B:False")
+Answer = (input("Enter Answer letter here:")).upper().strip()
+
+if (Answer == "A"):
+    print("Great job. Keep up the great work.")
+    
+elif (Answer == "B"):
+    print("Sorry, Inccorect. The correct answer was (A:True). Keep Trying. Don't let this put you down.")
+    
+print("Thanks for taking this quiz. Even if you didnt get all the answers right, at least you learned something.")
+print("Have a great rest of your day :)")
