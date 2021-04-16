b60a4dcb51d6aeebbaf817ffaf5a3c9296f70e6f
Sergei Issaev <53531737+sergeiissaev@users.noreply.github.com>
2021-04-16 11:17:50 -0400

    Create Adrian.py

---
+++ Adrian.py
@@ -0,0 +1,113 @@
+print("Welcome to my trivia ")
+print("Important all answers should be in lower case")
+print ("                   ")
+print ("Question 1")
+user_input=input("What is the capital of Canada:")
+if user_input== "ottawa":
+    print("Correct")
+else:
+   print("Incorrect")
+
+print ("                   ")
+
+print ("Question 2")
+user_input=input("When is the independence day of Canada:")
+if user_input== "july 1":
+    print("Correct")
+else:
+   print("Incorrect")
+   
+print("")
+
+print ("Question 3")
+user_input=input("Who is the owner of Testla:")
+if user_input== "elon musk":
+    print("Correct")
+else:
+   print("Incorrect")
+
+print ("                   ")
+
+print ("Question 4")
+user_input=input("What is the dialing code in canada:")
+if user_input== "+1":
+    print("Correct")
+else:
+   print("Incorrect")
+   
+print("")
+
+
+
+
+
+
+
+print ("Question 5")
+user_input=input("What is 7 to the power of 7:")
+if user_input== "823543":
+    print("Correct")
+else:
+   print("Incorrect")
+
+print ("                   ")
+
+print ("Question 6")
+user_input=input("Can  4524 be divide by 6 :")
+if user_input== "yes":
+    print("Correct")
+else:
+   print("Incorrect")
+   
+print("")
+
+print ("Question 7")
+user_input=input("What is 8378 x 78:")
+if user_input== " 653562":
+    print("Correct")
+else:
+   print("Incorrect")
+
+print ("                   ")
+
+print ("Question 8")
+user_input=input("what is 36489 + 83690:")
+if user_input== "120179":
+    print("Correct")
+else:
+   print("Incorrect")
+   
+print("")
+
+user_input=input("How many question did you get right:")
+
+
+if user_input == "0":
+    print ("Your IQ is below 1")
+if user_input == "1":
+    print ("Your IQ is below a 100")
+
+if user_input == "2":
+    print ("Your IQ is  80")
+
+if user_input == "3":
+    print ("Your IQ is 125")
+   
+if user_input == "4":
+    print ("Your IQ is  130")
+   
+if user_input == "5":
+    print ("Your IQ is  145")
+   
+if user_input == "6":
+    print ("Your IQ is  155")
+
+int=user_input
+if user_input == "7":
+    print ("Your IQ is  168")
+
+if user_input == "8":
+    print ("Your IQ is above 200")
+
+if user_input == "8" < "10":
+    print("Invalid!!!")
